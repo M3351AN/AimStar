fe1d906db2856926a7bd04f4d2b4f61e39736e89
root <root@localhost>
2024-02-14 00:33:12 +0800

    function pasted

--- CS2_External/Features/RCS.h
+++ CS2_External/Features/RCS.h
@@ -61,4 +61,82 @@ namespace RCS
 			Angles.y = PunchAngle.y;
 		}
 	}
+    inline void StandaloneRCS(const CEntity& Local, Vec3 LocalPos)
+	{
+       
+       float Yaw, Pitch;
+        float Distance, Norm, Length;
+        Vec3 AimPos, OppPos;
+        Vec2 Angles{ 0,0 };
+        int ScreenCenterX = Gui.Window.Size.x / 2;
+        int ScreenCenterY = Gui.Window.Size.y / 2;
+        float TargetX = 0.f;
+        float TargetY = 0.f;
+        float yawf = LocalEntity.Pawn.Fov.y * M_PI / 180;        
+        float pitchf = LocalEntity.Pawn.Fov.x * M_PI / 180;
+                
+AimPos.x = cosf(yawf) * sinf(pitchf);
+AimPos.y = sinf(yawf) * sinf(pitchf);
+AimPos.z = cosf(pitchf);
+
+
+        OppPos = AimPos - LocalPos;
+
+        Distance = sqrt(pow(OppPos.x, 2) + pow(OppPos.y, 2));
+
+        Length = sqrt(Distance * Distance + OppPos.z * OppPos.z);
+        // RCS by @Tairitsu modi. @_ukia_
+        if (MenuConfig::RCS)
+        {
+            RCS::UpdateAngles(Local, Angles);
+            float rad = Angles.x * RCSScale.x / 180.f * M_PI;
+            float si = sinf(rad);
+            float co = cosf(rad);
+
+            float z = OppPos.z * co + Distance * si;
+            float d = (Distance * co - OppPos.z * si) / Distance;
+
+            rad = -Angles.y * RCSScale.y / 180.f * M_PI;
+            si = sinf(rad);
+            co = cosf(rad);
+
+            float x = (OppPos.x * co - OppPos.y * si) * d;
+            float y = (OppPos.x * si + OppPos.y * co) * d;
+
+            OppPos = Vec3{ x, y, z };
+
+            AimPos = LocalPos + OppPos;
+        }
+
+        Yaw = atan2f(OppPos.y, OppPos.x) * 57.295779513 - Local.Pawn.ViewAngle.y;
+        Pitch = -atan(OppPos.z / Distance) * 57.295779513 - Local.Pawn.ViewAngle.x;
+        Norm = sqrt(pow(Yaw, 2) + pow(Pitch, 2));
+
+        Vec2 ScreenPos;
+        gGame.View.WorldToScreen(Vec3(AimPos), ScreenPos);
+        // Shake Fixed by @Sweely
+            if (ScreenPos.x != ScreenCenterX)
+            {
+                TargetX = (ScreenPos.x > ScreenCenterX) ? -(ScreenCenterX - ScreenPos.x) : ScreenPos.x - ScreenCenterX;
+                TargetX /= 1.5f;
+                TargetX = (TargetX + ScreenCenterX > ScreenCenterX * 2 || TargetX + ScreenCenterX < 0) ? 0 : TargetX;
+            }
+
+            if (ScreenPos.y != 0)
+            {
+                if (ScreenPos.y != ScreenCenterY)
+                {
+                    TargetY = (ScreenPos.y > ScreenCenterY) ? -(ScreenCenterY - ScreenPos.y) : ScreenPos.y - ScreenCenterY;
+                    TargetY /= 1.5f;
+                    TargetY = (TargetY + ScreenCenterY > ScreenCenterY * 2 || TargetY + ScreenCenterY < 0) ? 0 : TargetY;
+                }
+            }
+
+            if (!Smooth)
+            {
+                mouse_event(MOUSEEVENTF_MOVE, (DWORD)(TargetX), (DWORD)(TargetY), NULL, NULL);
+                return;
+            }
+
+    }
 }
